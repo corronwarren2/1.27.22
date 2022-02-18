430d89ac923864e0d3479b349807af738867520c
corronwarren2 <86121210+corronwarren2@users.noreply.github.com>
2022-02-17 20:02:20 -0800

    completed navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/assets/logo.svg';
 
 //BEM -> Block Element Modifier
 
 const Navbar = () => {
-    return (
-        <div className="gpt3__navbar">
-          <div className="gpt3__navbar-links">
-              <div className="gpt3__navbar-links_logo">
-                <img src={logo} alt="logo" />
-              </div>
-              <div className="gpt3__navbar-links_container">
-                <p><a href="#home">Home</a></p>
-                <p><a href="#wgpt3">What is GPT3?</a></p>
-                <p><a href="#possibility">Open AI</a></p>
-                <p><a href="#features">Case Studies</a></p>
-                <p><a href="#blog">Library</a></p>
-              </div>
-          </div>
-          <div className="gpt3__navbar-sign">
-              <p>Sign In</p>
-              <button type="button">Sign Up</button>
-          </div>
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
         </div>
-      );
-    }
+        <div className="gpt3__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign In</p>
+        <button type="button">Sign Up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+          {toggleMenu
+            ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+            : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+          }
+      </div>
+    </div>
+  );
+}
 
 export default Navbar;
